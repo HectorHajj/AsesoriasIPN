ac5f6c407b331dbccc52a38e1b4274ad6590a83a
Juls <i7ajacu@outlook.com>
2019-04-03 14:50:36 -0600

    Implementacion inicial de RankingBar

--- app/src/main/java/com/example/tutor40/Calificaciones.java
+++ app/src/main/java/com/example/tutor40/Calificaciones.java
@@ -4,56 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.RatingBar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Calificaciones extends AppCompatActivity {
     FirebaseAuth mAuth;
+    RatingBar R1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calificaciones);
+        RatingBar R1 = (RatingBar) findViewById (R.id. ratingBar );  // iniciar una barra de calificación
+    }
+
+    private void getRating() {
     }
 
     public void Continuar(View view){
+        getRating();
+
         Intent intent = new Intent(getApplicationContext(), TutorMain.class);
         startActivity(intent);
     }
-    //   startActivity(intent);
-    //
-    //@Override
-    //public void onStart() {
-       // super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        //try {
-
-          //  db = FirebaseFirestore.getInstance();
-
-            //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-            //Log.i("Login", user.getEmail());
-
-            //Si hay un usuario logeado
-            //DocumentReference docRef = db.collection("users").document(user.getUid());
-            //docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-              //  @Override
-               // public void onSuccess(DocumentSnapshot documentSnapshot) {
-                 //   if(documentSnapshot.getData().get("RolID").toString() == "Ck5Tnzr0ipmAzKpQpTDX") {
-                   //     Intent intent = new Intent(getApplicationContext(), TutorMain.class);
-
-                     //   startActivity(intent);
-                //    }
-                //}
-            //});
-
-        //} catch(Exception e){
-          //  e.printStackTrace();
-
-            //Los llevamos a Login
-            //Intent intent = new Intent(getApplicationContext(), Login.class);
-
-        //    startActivity(intent);
-        //}
-    //}
 }
