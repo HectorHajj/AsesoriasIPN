a6242513415b0d02fae97a8171ffad53e7274ffb
Eduardo Escobar <eescobarv1500@alumno.ipn.mx>
2019-05-28 17:20:29 -0500

    Reloj solo visible para el Usuario Asesor.

--- app/src/main/java/com/example/tutor40/Chat.java
+++ app/src/main/java/com/example/tutor40/Chat.java
@@ -103,7 +103,6 @@ public class Chat extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                Toast.makeText(Chat.this, currentUserRole.toString(), Toast.LENGTH_SHORT).show();
                 SaveMessageInfoToDatabase();
 
                 userMessageInput.setText("");
